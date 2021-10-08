62ae459ae3ce8683fe093bc76818bb54be9d10b8
Tomas Fagerbekk <tomas.a.fagerbekk@gmail.com>
2021-10-08 12:21:17 +0200

    fix: clear apollo cache on logout
    For security (and debugging) reasons.

--- pages/logout.tsx
+++ pages/logout.tsx
@@ -2,14 +2,17 @@ import { useEffect } from "react";
 import { useRouter } from "next/router";
 import auth from "next-auth/client";
 import {ROUTES} from "../utils/routes.utils";
+import {useApolloClient} from "@apollo/client";
 
 export default function LogoutPage() {
   const [session, isLoading] = auth.useSession();
+  const client = useApolloClient();
   const router = useRouter();
   useEffect(() => {
     if (isLoading) {
       return;
     }
+    client.clearStore();
     if (session) {
       auth.signOut();
     }
